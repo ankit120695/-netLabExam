14b54e554f28e1e2304c5ad93056afd53f1db69f
ankit120695 <122710945+ankit120695@users.noreply.github.com>
2023-02-05 15:59:34 +0530

    Add files via upload

---
+++ Q1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[10];
+            Random random = new Random();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(1, 50);
+            }
+
+            var evenNumbers = from number in numbers
+                              where number % 2 == 0
+                              select number;
+
+            Console.WriteLine("Numbers divisible by 2:");
+            foreach (int evenNumber in evenNumbers)
+            {
+                Console.WriteLine(evenNumber);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Q2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 10;
+            int n2 = 20;
+
+            IncrementAndDecrement(ref n1, ref n2);
+
+            Console.WriteLine("n1: " + n1);
+            Console.WriteLine("n2: " + n2);
+
+            Console.ReadKey();
+        }
+
+        static void IncrementAndDecrement(ref int n1, ref int n2)
+        {
+            n1++;
+            n2--;
+        }
+    }
+}
